7224a9c077a2bda053801f1d9ca80b85667b52c1
Joao Marcos <joaomarcos11.tech@gmail.com>
2021-01-29 20:36:58 -0300

    SPT01-2021: css global e outras configs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Router } from 'react-router-dom';
 import Routes from './routes';
 import history from './services/history';
 
+import './assets/styles/global.css';
+
 
 export default function App() {
   return (
